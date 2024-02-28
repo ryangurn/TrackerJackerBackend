eb4afa993b151342999d66736c0e100b9e1af507
Ryan Gurnick <ryangurnick@gmail.com>
2024-02-28 15:32:22 -0800

    Adding operating system version tables

---
+++ database/migrations/2024_02_25_064718_create_versions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\System;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('versions', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->foreignIdFor(System::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('version');
+            $table->string('code_name')
+                ->nullable();
+            $table->boolean('supported');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('versions');
+    }
+};
